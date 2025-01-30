f2a15a13489f932f1a8212b427a83ebb6b310dad
Ildar Shamiev <shamief@yandex.ru>
2025-01-30 12:59:48 +0300

    game events

--- src/features/game/model/events.ts
+++ src/features/game/model/events.ts
@@ -2,18 +2,33 @@ import { createAction } from "@reduxjs/toolkit";
 import { GameField } from "./domain/game-field";
 import { GameHistory } from "./domain/game-history";
 import { GameStatusGameOver, GameStatusInProgress } from "./domain/game-status";
+import { GameTimers } from "./domain/game-timers";
+
+export const gameStartedEvent = createAction<{
+  gameStatus: GameStatusInProgress;
+}>("event/game/started");
+
+
+export const timeOverEvent = createAction<{
+  gameStatus: GameStatusInProgress;
+  gameTimers: GameTimers;
+  gameField: GameField;
+}>("event/game/time-over");
 
 export const moveCompletedEvent = createAction<{
   gameField: GameField;
   gameStatus: GameStatusInProgress;
+  gameTimers: GameTimers;
 }>("event/game/move-completed");
 
 export const gameOverEvent = createAction<{
   gameField: GameField;
   gameStatus: GameStatusGameOver;
+  gameTimers: GameTimers;
 }>("event/game/over");
 
 export const historyViewedEvent = createAction<GameHistory>(
   "event/game/history-viewed",
 );
+
 export const gameViewedEvent = createAction("event/game/game-viewed");
